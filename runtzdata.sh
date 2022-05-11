52128971296237bb81a09f313d30fd83ad3767f9
koyu <me@koyu.space>
2022-05-11 22:26:38 +0200

    Better tzdata UX

--- runtzdata.sh
+++ runtzdata.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-feh --bg-scale /home/pi/feldberg/bootsplash.png
+feh --bg-scale /home/pi/feldberg/bootsplash.png &
 killall surf
-lxterminal -e "sudo dpkg-reconfigure tzdata"
-surf http://localhost:5000 &
+lxterminal -e "sudo dpkg-reconfigure tzdata && surf http://localhost:5000 &"
 feh --bg-scale /home/pi/feldberg/crash.png &
