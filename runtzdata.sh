e3b37ae7e666c384ada0da1e5d7e21c895554f38
koyu <me@koyu.space>
2024-07-30 15:21:04 +0200

    Some shit

--- runtzdata.sh
+++ runtzdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-feh --bg-scale /home/pi/feldberg/bootsplash.png &
+feh --bg-scale /home/pi/firmware/bootsplash.png &
 killall surf
 lxterminal -e "sudo dpkg-reconfigure tzdata" && /home/pi/init.sh
-feh --bg-scale /home/pi/feldberg/crash.png &
+feh --bg-scale /home/pi/firmware/crash.png &
